e056b989779d39d7dea45860feec93a7cf922003
Marcos Barrera <mabarrera@csumb.edu>
2020-04-04 13:03:14 -0700

    Update main of M5_LowCard_Phase2.java

--- src/M5_LowCard_Phase2.java
+++ src/M5_LowCard_Phase2.java
@@ -8,22 +8,64 @@
  * 04/07/2020
  */
 
-import javax.swing.JFrame;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
 
 public class M5_LowCard_Phase2 {
+   static int NUM_CARDS_PER_HAND = 7;
+   static int NUM_PLAYERS = 2;
+   static JLabel[] computerLabels = new JLabel[NUM_CARDS_PER_HAND];
+   static JLabel[] humanLabels = new JLabel[NUM_CARDS_PER_HAND];
+   static JLabel[] playedCardLabels = new JLabel[NUM_PLAYERS];
+   static JLabel[] playLabelText = new JLabel[NUM_PLAYERS];
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		  ImageIcon icon = new ImageIcon("images/2C.gif");
-		  JLabel label = new JLabel(icon);
-		  frame.add(label);
-		  frame.setDefaultCloseOperation
-		         (JFrame.EXIT_ON_CLOSE);
-		  frame.pack();
-		  frame.setVisible(true);
+   public static void main(String[] args) {
+      int k;
+      Icon tempIcon;
+      // establish main frame in which program will run
+      CardTable myCardTable = new CardTable("CardTable", 
+                                             NUM_CARDS_PER_HAND,
+                                             NUM_PLAYERS);
+      myCardTable.setSize(800, 600);
+      myCardTable.setLocationRelativeTo(null);
+      myCardTable.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	}
+      // show everything to the user
+      myCardTable.setVisible(true);
 
+      /*
+       * CREATE LABELS ----------------------------------------------------
+       * code goes here ...
+       */
+      
+      /*
+       * ADD LABELS TO PANELS -----------------------------------------
+       * code goes here ...
+       */
+
+      /*and two random cards in the play region 
+       * (simulating a computer/human play)
+       * code goes here ...
+       */
+
+      // show everything to the user
+      myCardTable.setVisible(true);
+
+   }
+
+}
+
+class CardTable extends JFrame {
+   public CardTable(String string, int nUM_CARDS_PER_HAND, int nUM_PLAYERS) {
+      // TODO Auto-generated constructor stub
+   }
+
+   static int MAX_CARDS_PER_HAND = 56;
+   static int MAX_PLAYERS = 2; 
+
+   private int numCardsPerHand;
+   private int numPlayers;
+
+   public JPanel pnlComputerHand, pnlHumanHand, pnlPlayArea;
 }
